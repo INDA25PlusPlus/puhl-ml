aeff2d1588a80d450368c589d680d50bfc000c3e
Jakob Puhl <puhl@kth.se>
2025-11-18 16:35:43 +0100

    Add batching to ReLU

--- src/nn/activation_layers.rs
+++ src/nn/activation_layers.rs
@@ -1,19 +1,19 @@
-use nalgebra::SVector;
+use nalgebra::{SMatrix};
 use super::layer::Layer;
 
-pub struct ReLU<const I: usize> {
-    last_input: Option<SVector<f64, I>>,
+pub struct ReLU<const B: usize, const I: usize> {
+    last_input: Option<SMatrix<f64, I, B>>,
 }
 
-impl<const I: usize> ReLU<I> {
+impl<const B: usize, const I: usize> ReLU<B, I> {
     pub fn new() -> Self {
         Self { last_input: None }
     }
 }
 
-impl<const I: usize> Layer for ReLU<I> {
-    type Input = SVector<f64, I>;
-    type Output = SVector<f64, I>;
+impl<const B: usize, const I: usize> Layer for ReLU<B, I> {
+    type Input = SMatrix<f64, I, B>;
+    type Output = SMatrix<f64, I, B>;
 
     fn forward(&mut self, input: &Self::Input) -> Self::Output {
         self.last_input = Some(*input);
